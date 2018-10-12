470d560256e1c44b38172b1fb85332f72423cccb
weiguow <2573777501@qq.com>
2018-10-12 09:42:12 +0800

    glog

--- src/wang_wei_guo/glogTest/main.cpp
+++ src/wang_wei_guo/glogTest/main.cpp
@@ -2,7 +2,8 @@
 #include <glog/logging.h>
 int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
-    google::SetLogDestination(google::INFO, "./Log");
+    google::SetLogDestination(google::INFO, "/home/weiguow/project/PeparationLearning/src"
+                                            "/wang_wei_guo/glogTest/log");
     LOG(INFO) << "HELLO" << "ok!";
     LOG(WARNING) << "warning test";
     return 0;
