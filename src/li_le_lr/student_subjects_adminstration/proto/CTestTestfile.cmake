784f81cc82f8260b6bf1550a2706320b1869cf00
lilelr <lilelr@163.com>
2018-10-31 14:56:15 +0800

    3rparty

---
+++ src/li_le_lr/student_subjects_adminstration/proto/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lilelr/open-source/bak_mesos-1.3.2/my_hello/proto
+# Build directory: /home/lilelr/open-source/bak_mesos-1.3.2/my_hello/proto
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
